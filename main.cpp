5bcd4fbbb73ba8dd5b5a50797f6de445e8b975ea
aiden cox <cox.378@wright.edu>
2025-03-05 16:37:06 -0500

    defining funtions

--- main.cpp
+++ main.cpp
@@ -1,6 +1,43 @@
 #include <iostream>
 
+//reads top to bottom, you have to have at least a skeleton before, and then you can put the rest on the bottom
+
+void happyBirthday(std::string name, int age);
+
+double square(double length);
+
+std::string concatStrings(std::string string1, std::string string2);
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    happyBirthday("aiden", 20);
+
+    double length = 5.0;
+    double area = square(length);
+
+    std::cout << "\nArea: " << area << "cm squared\n\n";
+
+
+    std::string firstName = "Aiden";
+    std::string lastName = "Cox";
+    std::string fullName = concatStrings(firstName, lastName);
+
+    std::cout << "Hello " << fullName << '\n';
+
     return 0;
+}
+void happyBirthday(std::string name, int age) {
+    std::cout << "Happy Birthday to " << name << "!\n";
+    std::cout << "Happy Birthday to " << name << "!\n";
+    std::cout << "Happy Birthday dear " << name << "!\n";
+    std::cout << "Happy Birthday to " << name << "!\n";
+    std::cout << "You are " << age << " years old!\n";
+}
+
+double square(double length) {
+    double result = length * length;
+    return result;
+}
+
+std::string concatStrings(std::string string1, std::string string2) {
+    return string1 + " " + string2;
 }
